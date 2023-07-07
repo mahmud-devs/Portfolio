dc3483889d4e1034d0f9bee77d784a3da46ad3a1
mahmud-devs <mahmudulislam0999@gmail.com>
2023-07-07 07:44:00 +0600

    New added

--- js/script.js
+++ js/script.js
@@ -60,3 +60,25 @@ $(function(){
 		resetCallback: function() {}
 	});
 });
+
+// ============= slick slider ==========
+$('.slick__slider').slick({
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: false,
+	// centerMode: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  slidesToShow: 1,
+		  autoplay: true,
+		  autoplaySpeed: 2500,
+		}
+	  }
+	]
+  });
