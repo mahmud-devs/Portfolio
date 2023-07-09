be458b054f83eb3e58f3158925700214c28041ad
mahmud-devs <mahmudulislam0999@gmail.com>
2023-07-10 00:30:45 +0600

    contact part added

--- js/script.js
+++ js/script.js
@@ -108,3 +108,96 @@ $('.slick__slider').slick({
 	  }
 	]
   });
+
+
+
+
+  // ========= form validation ======
+let username = document.querySelector(".name");
+let email = document.querySelector(".email");
+let subject = document.querySelector(".subject");
+let text = document.querySelector(".text");
+let btn = document.querySelector(".form__btn");
+let msg = document.querySelector(".error__msg");
+let mail_valid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+let nameerr = document.querySelector(".name__err");
+let mailerr = document.querySelector(".mail__err");
+let suberr = document.querySelector(".sub__err");
+let texterr = document.querySelector(".text__err");
+
+
+
+
+function SendEmail() {
+  if (username.value === "") {
+    nameerr.innerHTML = "Type your full name";
+    nameerr.style.color = "red";
+    username.style.borderBottom = "2px solid red";
+  } else {
+    nameerr.innerHTML = "";
+  }
+
+  if (email.value === "") {
+    mailerr.innerHTML = "Type your valid email";
+    mailerr.style.color = "red";
+  } else {
+    if (email.value.match(mail_valid)) {
+      mailerr.innerHTML = "";
+    } else {
+      mailerr.innerHTML = "Your email is not valid";
+
+    }
+  }
+
+  if (subject.value === "") {
+    suberr.innerHTML = "Type a subject";
+    suberr.style.color = "red";
+  } else {
+    suberr.innerHTML = "";
+  }
+
+  if (text.value === "") {
+    texterr.innerHTML = "Type your message";
+    texterr.style.color = "red";
+  } else {
+    texterr.innerHTML = "";
+  }
+  // Create an object to store the values
+  var bodyValues = {
+    "Name": username.value,
+    "Email": email.value,
+    "Subject": subject.value,
+    "Message": text.value,
+  };
+console.log(bodyValues);
+  // Format the values in the email body
+  var body = "";
+  for (var key in bodyValues) {
+    if (bodyValues.hasOwnProperty(key)) {
+      body += key + ": " + bodyValues[key] + "<br>";
+    }
+  }
+  // Check if all fields are valid before sending the email
+  if (nameerr.innerHTML === "" && mailerr.innerHTML === "" && suberr.innerHTML === "" && texterr.innerHTML === "") {
+    // Configure SMTPJS with your SMTP service provider credentials
+    Email.send({
+      SecureToken: "4e8111f5-eaf4-4d98-aeb7-392791102ad2", // Replace with your SMTPJS token
+      To: "mahmudulislam0999@gmail.com",
+      From: "mahmudulislam0999@gmail.com",
+      Subject: subject.value,
+      Body: body,
+    }).then(function (message) {
+      if (message === "OK") {
+        alert("Email sent successfully!");
+        // Clear input fields
+        username.value = "";
+        email.value = "";
+        subject.value = "";
+        text.value = "";
+      } else {
+        alert("Error sending email. Please try again later.");
+      }
+    });
+  }
+}
